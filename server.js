610017c1e8ba4394e32cd20e83ee22bfee163f32
rehanmohammed20269-star <rehanmohammed20269@gmail.com>
2026-04-01 14:55:34 +0530

    Initialize Express server with basic route

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("SunLo server running 💙");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
